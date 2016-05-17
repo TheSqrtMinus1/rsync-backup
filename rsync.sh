0d95f6fd75f1301e6d9b6a67af1a781771f65d07
gabe <thesqrtminus1@gmail.com>
2016-05-17 12:28:50 +0000

    ACTUALLY added script

--- rsync.sh
+++ rsync.sh
@@ -1,1 +1,4 @@
-/scripts/rsync.sh
+#!/bin/bash
+tmux kill-session -t rsync ; tmux new-session -d -s rsync ;
+tmux send-keys -t rsync "sudo rsync -aS -HAX -vh -uz --compress-level=9 --progress --rsync-path=\"sudo rsync\" --delete --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found","/back/*"} -e \"ssh -l gabe -i /home/gabe/.ssh/id_rsa\" / /back" ENTER
+exit 0
