2f84addf83a77b047d7586e4320353b85fb76970
steph <thesqrtminus1@gmail.com>
2018-01-07 02:33:05 -0500

    Update rsync.sh

--- rsync.sh
+++ rsync.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 tmux kill-session -t rsync ; tmux new-session -d -s rsync ;
-tmux send-keys -t rsync "sudo rsync -aS -HAX -vh -uz --compress-level=9 --progress --rsync-path=\"sudo rsync\" --delete --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found","/back/*"} -e \"ssh -l steph -i /home/gabe/.ssh/id_rsa\" / /back" ENTER
+tmux send-keys -t rsync "sudo rsync -aS -HAX -vh -uz --compress-level=9 --progress --rsync-path=\"sudo rsync\" --delete --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found","/back/*"} -e \"ssh -l steph -i /home/steph/.ssh/id_rsa\" / /back" ENTER
 exit 0
